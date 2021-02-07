b5ba9e4f6522a7790728124de1177fb22027c579
JiaLee <s2018s37@e-mirim.hs.kr>
2021-02-07 23:03:10 +0900

    image-slide 제작(위치 조정 필요)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+// let jsdom = require('jsdom'); 
+// let window = jsdom.jsdom().createWindow();
+// let $ = require('jquery')(window);
+// console.log('hello');
+$(document).ready(function () {
+    var options = {
+        start: 0,
+        fadeIn: 400,
+        loop: true,
+        buttonPrev: 'Previous',
+        buttonNext: 'Next',
+        style: 'carousel',
+        spacing: -0.6,
+        nav: false,
+        buttons: true
+    };
+    $('.my-flipster').flipster(options);
+});
